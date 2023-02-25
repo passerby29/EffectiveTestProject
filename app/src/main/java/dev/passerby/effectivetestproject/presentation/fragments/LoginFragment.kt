2e3e370d696eb955f833901b6e0c770322d96fb7
passerby29 <grtte@list.ru>
2023-02-25 20:54:25 +0300

    finishing start activity design

--- app/src/main/java/dev/passerby/effectivetestproject/presentation/fragments/LoginFragment.kt
+++ app/src/main/java/dev/passerby/effectivetestproject/presentation/fragments/LoginFragment.kt
@@ -1,15 +1,12 @@
 package dev.passerby.effectivetestproject.presentation.fragments
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import dev.passerby.effectivetestproject.databinding.FragmentLoginBinding
-import dev.passerby.effectivetestproject.databinding.FragmentSignInBinding
 import dev.passerby.effectivetestproject.presentation.activities.MainActivity
 
 class LoginFragment : Fragment() {
--- app/src/main/java/dev/passerby/effectivetestproject/presentation/fragments/SignInFragment.kt
+++ app/src/main/java/dev/passerby/effectivetestproject/presentation/fragments/SignInFragment.kt
@@ -1,12 +1,14 @@
 package dev.passerby.effectivetestproject.presentation.fragments
 
 import android.os.Bundle
+import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import dev.passerby.effectivetestproject.R
 import dev.passerby.effectivetestproject.databinding.FragmentSignInBinding
+
 
 class SignInFragment : Fragment() {
 
@@ -23,9 +25,16 @@ class SignInFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.signInBtn.setOnClickListener {
-            parentFragmentManager.beginTransaction().replace(R.id.startContainer, LoginFragment())
-                .commit()
+        binding.signItHaveAccountTv.apply {
+            setOnClickListener {
+                parentFragmentManager.beginTransaction()
+                    .replace(R.id.startContainer, LoginFragment())
+                    .commit()
+            }
+
+            val first = "Already have an account? "
+            val next = "<font color='#254FE6'>Log in!</font>"
+            text = Html.fromHtml(first + next, 0)
         }
     }
 }
