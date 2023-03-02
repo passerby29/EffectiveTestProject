1e88ed68f6705af93008c5c9ee407ab55c14f2a5
passerby29 <grtte@list.ru>
2023-03-02 15:19:57 +0300

    Adding Log out function

--- app/src/main/java/dev/passerby/effectivetestproject/presentation/fragments/ProfileFragment.kt
+++ app/src/main/java/dev/passerby/effectivetestproject/presentation/fragments/ProfileFragment.kt
@@ -1,33 +1,34 @@
 package dev.passerby.effectivetestproject.presentation.fragments
 
-import androidx.lifecycle.ViewModelProvider
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import dev.passerby.effectivetestproject.R
-import dev.passerby.effectivetestproject.presentation.viewmodels.ProfileViewModel
+import dev.passerby.effectivetestproject.databinding.FragmentProfileBinding
+import dev.passerby.effectivetestproject.presentation.activities.StartActivity
 
 class ProfileFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = ProfileFragment()
-    }
-
-    private lateinit var viewModel: ProfileViewModel
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.profileLogoutBtn.setOnClickListener {
+            val intent = Intent(requireContext(), StartActivity::class.java)
+            startActivity(intent)
+        }
     }
-
 }
