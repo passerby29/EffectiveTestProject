cd1b5bc7bd30058f2463f7d4cc414c3de00fef83
passerby29 <grtte@list.ru>
2023-03-03 14:33:28 +0300

    Adding use cases to viewModel

--- app/src/main/java/dev/passerby/effectivetestproject/presentation/viewmodels/HomeAViewModel.kt
+++ app/src/main/java/dev/passerby/effectivetestproject/presentation/viewmodels/HomeAViewModel.kt
@@ -5,16 +5,22 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import dev.passerby.effectivetestproject.data.impls.MainRepositoryImpl
+import dev.passerby.effectivetestproject.data.room.SearchWordsEntity
 import dev.passerby.effectivetestproject.data.server.BaseResponse
 import dev.passerby.effectivetestproject.domain.models.SearchWords
+import dev.passerby.effectivetestproject.domain.usecases.AddSearchWordsToDbUseCase
+import dev.passerby.effectivetestproject.domain.usecases.GetSearchWordsFromDbUseCase
 import dev.passerby.effectivetestproject.domain.usecases.GetSearchWordsUseCase
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class HomeAViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val repository = MainRepositoryImpl
+    private val repository = MainRepositoryImpl(application)
 
     private val getSearchWordsUseCase = GetSearchWordsUseCase(repository)
+    private val addSearchWordToDbUseCase = AddSearchWordsToDbUseCase(repository)
+    private val getSearchWordsFromDbUseCase = GetSearchWordsFromDbUseCase(repository)
 
     var result: MutableLiveData<BaseResponse<SearchWords>> = MutableLiveData()
 
@@ -22,7 +28,7 @@ class HomeAViewModel(application: Application) : AndroidViewModel(application) {
         result.value = BaseResponse.Loading()
         viewModelScope.launch {
             try {
-                val response = repository.getSearchWords()
+                val response = getSearchWordsUseCase.getSearchWords()
                 if (response?.code() == 200) {
                     result.value = BaseResponse.Success(response.body())
                 } else {
@@ -32,5 +38,19 @@ class HomeAViewModel(application: Application) : AndroidViewModel(application) {
                 result.value = BaseResponse.Error(ex.message)
             }
         }
+    }
+
+    fun addSearchWord(searchWord: SearchWordsEntity) {
+        viewModelScope.launch(Dispatchers.IO) {
+            addSearchWordToDbUseCase.addSearchWordsToDB(searchWord)
+        }
+    }
+
+    fun getSearchWordsFromDG(filter: String): List<SearchWordsEntity> {
+        var list: List<SearchWordsEntity> = emptyList()
+        viewModelScope.launch(Dispatchers.IO) {
+             list = getSearchWordsFromDbUseCase.getSearchWordsFromDB(filter)
+        }
+        return list
     }
 }
